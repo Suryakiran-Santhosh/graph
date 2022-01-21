d627cbe51526190af6514a2ef5a4adf48e5c34ab
Suryakiran Santhosh <surya@Suryakirans-MacBook-Pro.local>
2022-01-20 22:42:34 -0800

    finished the graph class and tested it.

---
+++ graph/Graph.py
@@ -0,0 +1,21 @@
+
+
+class Graph:
+    
+    def __init__(self):
+        self.graph = {}
+    
+    def add_edge(self, start : int, end : int) -> None:
+        if start not in self.graph:
+            self.add_vertex(start)
+        
+        if end not in self.graph[start]:
+            self.graph[start].add(end) 
+        
+    def add_vertex(self, vertex : int) -> None:
+        if vertex not in self.graph:
+            self.graph[vertex] = set()
+
+    def display(self) -> None:
+        for key, value in self.graph.items():
+            print(f"{key} : {list(value)}")
---
+++ main.py
@@ -0,0 +1,24 @@
+import graph.Graph as Graph
+
+def main():
+    graph = Graph.Graph()
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(2, 3)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(4, 7)
+    graph.add_edge(4, 8)
+    graph.add_edge(4, 6)
+    graph.add_edge(4, 5)
+    graph.add_edge(5, 6)
+    graph.add_edge(6, 9)
+    graph.add_edge(9, 8)
+    graph.add_vertex(8)
+    graph.add_vertex(7)
+    graph.display()
+
+
+if __name__ == "__main__":
+    main()
