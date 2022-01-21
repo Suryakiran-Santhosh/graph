2a98fc853e6727d1e0a921d63ed9088284b370d8
Suryakiran Santhosh <surya@Suryakirans-MacBook-Pro.local>
2022-01-20 22:49:45 -0800

    renamed variables

--- graph/Graph.py
+++ graph/Graph.py
@@ -5,17 +5,18 @@ class Graph:
     def __init__(self):
         self.graph = {}
     
-    def add_edge(self, start : int, end : int) -> None:
-        if start not in self.graph:
-            self.add_vertex(start)
+    def add_edge(self, vertex : int, neighbor : int) -> None:
+        if vertex not in self.graph:
+            self.add_vertex(vertex)
         
-        if end not in self.graph[start]:
-            self.graph[start].add(end) 
+        if neighbor not in self.graph[vertex]:
+            self.graph[vertex].add(neighbor) 
         
     def add_vertex(self, vertex : int) -> None:
         if vertex not in self.graph:
             self.graph[vertex] = set()
 
     def display(self) -> None:
-        for key, value in self.graph.items():
-            print(f"{key} : {value}")
+        for vertex, neighbors in self.graph.items():
+            print(f"{vertex} : {neighbors}")
+            
